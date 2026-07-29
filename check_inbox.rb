4682f616a29ab9f3d15b7bbbb87a73bdd65dd2a4
Digilunar <novian@digilunar.com>
2026-07-29 17:57:13 +0700

    chore: update script

--- check_inbox.rb
+++ check_inbox.rb
@@ -1,6 +1,4 @@
-puts "Inbox 2:"
-puts Inbox.find_by(id: 2).inspect
-puts "Account 1 Inboxes:"
-puts Account.find_by(id: 1)&.inboxes&.pluck(:id, :name).inspect
-puts "AgentBots:"
-puts AgentBot.all.pluck(:id, :name).inspect
+puts "All Accounts:"
+puts Account.unscoped.pluck(:id, :name).inspect
+puts "All Inboxes:"
+puts Inbox.unscoped.pluck(:id, :name, :account_id).inspect
